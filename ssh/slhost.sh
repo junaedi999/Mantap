4a067f7bba2341f3c3a4798955659bfc6df8a02a
anon999 <agustiar907@gmail.com>
2022-12-02 23:52:05 +0700

    Update slhost.sh

--- ssh/slhost.sh
+++ ssh/slhost.sh
@@ -20,11 +20,11 @@ echo "$SUB_DOMAIN" >> /etc/v2ray/domain
 #
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=zerosl-${sub}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
-CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
+DOMAIN=anabulbul.xyz
+SUB_DOMAIN=akun-${sub}.anabulbul.xyz
+NS_DOMAIN=zerosl-${sub}.anabulbul.xyz
+CF_ID=iis.ismawati0709@gmail.com
+CF_KEY=550223f4b3ac98c7a23a6cffd47673117d35a
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
